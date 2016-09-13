2a819c319723495696b3798ea48a5ab953ec6fab
Mikael Karon <mikael@karon.se>
2016-09-13 18:45:45 +0200

    bump deps Bumped dependencies and updated code, also lock mu-jquery-app so we're not on a moving target.

--- examples/mu-jquery-app/js/app.js
+++ examples/mu-jquery-app/js/app.js
@@ -6,7 +6,7 @@
     }));
 })([
     "jquery",
-    "mu-jquery-widget/jquery.weave",
+    "mu-jquery-component/jquery.weave",
     "mu-jquery-hub/hub"
 ], this, function (jQuery, weave, hub) {
     var slice = Array.prototype.slice;
