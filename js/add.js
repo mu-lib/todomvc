a24b958f8fd0c3b6416ff9fc65b76b6096f928d0
Mikael Karon <mikael@karon.se>
2017-03-18 17:34:33 +0100

    formatting

--- js/add.js
+++ js/add.js
@@ -1,34 +1,31 @@
 (function (modules, root, factory) {
-  if (typeof define === "function" && define.amd) {
-    define(modules, factory);
-  } else if (typeof module === "object" && module.exports) {
-    module.exports = factory.apply(root, modules.map(require));
-  } else {
-    root["todos/add"] = factory.apply(root, modules.map(function (m) {
-      return this[m] || root[m.replace(/^\./, "todos")];
-    }));
-  }
-})([
-  "./create",
-  "mu-jquery-app-hub/widget"
-], this, function (create, widget) {
-  var enter = 13;
+	if (typeof define === "function" && define.amd) {
+		define(modules, factory);
+	} else if (typeof module === "object" && module.exports) {
+		module.exports = factory.apply(root, modules.map(require));
+	} else {
+		root["todos/add"] = factory.apply(root, modules.map(function (m) {
+			return this[m] || root[m.replace(/^\./, "todos")];
+		}));
+	}
+})(["./create", "mu-jquery-app-hub/widget"], this, function (create, widget) {
+	var enter = 13;
 
-  return create(widget, {
-    "on/keyup": function ($event) {
-      var me = this;
-      var value;
+	return create(widget, {
+		"on/keyup": function ($event) {
+			var me = this;
+			var value;
 
-      if ($event.keyCode === enter) {
-        value = me.$element
-          .val()
-          .trim();
+			if ($event.keyCode === enter) {
+				value = me.$element
+					.val()
+					.trim();
 
-        if (value !== "") {
-          me.$element.val("");
-          me.publish("todos/add", value);
-        }
-      }
-    }
-  });
+				if (value !== "") {
+					me.$element.val("");
+					me.publish("todos/add", value);
+				}
+			}
+		}
+	});
 });
--- js/hide.js
+++ js/hide.js
@@ -1,20 +1,17 @@
 (function (modules, root, factory) {
-  if (typeof define === "function" && define.amd) {
-    define(modules, factory);
-  } else if (typeof module === "object" && module.exports) {
-    module.exports = factory.apply(root, modules.map(require));
-  } else {
-    root["todos/hide"] = factory.apply(root, modules.map(function (m) {
-      return this[m] || root[m.replace(/^\./, "todos")];
-    }));
-  }
-})([
-  "./create",
-  "mu-jquery-app-hub/widget"
-], this, function (create, widget) {
-  return create(widget, {
-    "hub/todos/change": function (tasks) {
-      this.$element.toggle(tasks.length !== 0);
-    }
-  });
+	if (typeof define === "function" && define.amd) {
+		define(modules, factory);
+	} else if (typeof module === "object" && module.exports) {
+		module.exports = factory.apply(root, modules.map(require));
+	} else {
+		root["todos/hide"] = factory.apply(root, modules.map(function (m) {
+			return this[m] || root[m.replace(/^\./, "todos")];
+		}));
+	}
+})(["./create", "mu-jquery-app-hub/widget"], this, function (create, widget) {
+	return create(widget, {
+		"hub/todos/change": function (tasks) {
+			this.$element.toggle(tasks.length !== 0);
+		}
+	});
 });
