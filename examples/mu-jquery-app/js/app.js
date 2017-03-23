f16f1598cda7d08bf48161296833fdb0d1a32266
Mikael Karon <mikael@karon.se>
2017-03-23 18:01:24 +0530

    formatting and bump mu-jquery-app-hub@6.0.2

--- examples/mu-jquery-app/js/app.js
+++ examples/mu-jquery-app/js/app.js
@@ -1,24 +1,24 @@
 (function (modules, root, factory) {
-	root["todos/app"] = factory.apply(root, modules.map(function (m) {
-		return this[m] || root[m.replace(/^\./, "todos")];
-	}, {
-			"jquery": root.jQuery
-		}));
+  root["todos/app"] = factory.apply(root, modules.map(function (m) {
+    return this[m] || root[m.replace(/^\./, "todos")];
+  }, {
+      "jquery": root.jQuery
+    }));
 })(["jquery", "mu-jquery-loom/jquery.loom", "mu-jquery-hub/hub"], this, function (jQuery, loom, hub) {
-	var root = this;
+  var root = this;
 
-	jQuery.fn.loom = loom;
+  jQuery.fn.loom = loom;
 
-	function load(module) {
-		return root[module];
-	}
+  function load(module) {
+    return root[module];
+  }
 
-	jQuery(function ($) {
-		$(document)
-			.loom("[mu-widget]", "mu-widget", load, {
-				"hub": hub.call($, "memory", "stopOnFalse")
-			})
-			.weave()
-			.fail(console.error.bind(console));
-	});
+  jQuery(function ($) {
+    $(document)
+      .loom("[mu-widget]", "mu-widget", load, {
+        "hub": hub.call($, "memory", "stopOnFalse")
+      })
+      .weave()
+      .fail(console.error.bind(console));
+  });
 });
