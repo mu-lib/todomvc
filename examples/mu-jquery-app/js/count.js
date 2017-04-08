9fed33b8f1af740d7ca66a3381ea9e3c0a77ba03
Mikael Karon <mikael@karon.se>
2017-04-08 15:13:37 +0200

    remove unused UMD parts

--- examples/mu-jquery-app/js/count.js
+++ examples/mu-jquery-app/js/count.js
@@ -1,23 +1,17 @@
 (function (modules, root, factory) {
-	if (typeof define === "function" && define.amd) {
-		define(modules, factory);
-	} else if (typeof module === "object" && module.exports) {
-		module.exports = factory.apply(root, modules.map(require));
-	} else {
-		root["todos/count"] = factory.apply(root, modules.map(function (m) {
-			return this[m] || root[m.replace(/^\./, "todos")];
-		}));
-	}
+  root["todos/count"] = factory.apply(root, modules.map(function (m) {
+    return this[m] || root[m.replace(/^\./, "todos")];
+  }));
 })(["./widget"], this, function (widget) {
-	return widget.extend({
-		"hub/todos/change": function (tasks) {
-			var count = tasks
-				.filter(function (task) {
-					return !task.completed;
-				})
-				.length;
+  return widget.extend({
+    "hub/todos/change": function (tasks) {
+      var count = tasks
+        .filter(function (task) {
+          return !task.completed;
+        })
+        .length;
 
-			this.$element.html("<strong>" + count + "</strong> " + (count === 1 ? "item" : "items") + " left");
-		}
-	});
+      this.$element.html("<strong>" + count + "</strong> " + (count === 1 ? "item" : "items") + " left");
+    }
+  });
 });
--- examples/mu-jquery-app/js/hide.js
+++ examples/mu-jquery-app/js/hide.js
@@ -1,17 +1,11 @@
 (function (modules, root, factory) {
-	if (typeof define === "function" && define.amd) {
-		define(modules, factory);
-	} else if (typeof module === "object" && module.exports) {
-		module.exports = factory.apply(root, modules.map(require));
-	} else {
-		root["todos/hide"] = factory.apply(root, modules.map(function (m) {
-			return this[m] || root[m.replace(/^\./, "todos")];
-		}));
-	}
+  root["todos/hide"] = factory.apply(root, modules.map(function (m) {
+    return this[m] || root[m.replace(/^\./, "todos")];
+  }));
 })(["./widget"], this, function (widget) {
-	return widget.extend({
-		"hub/todos/change": function (tasks) {
-			this.$element.toggle(tasks.length !== 0);
-		}
-	});
+  return widget.extend({
+    "hub/todos/change": function (tasks) {
+      this.$element.toggle(tasks.length !== 0);
+    }
+  });
 });
