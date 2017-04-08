f17a15ab45e06822706bd709dd9f006080476eea
Mikael Karon <mikael@karon.se>
2017-04-08 14:37:44 +0200

    bump deps and use new add signature
    - mu-jquery-app-hub@8.0.0

--- js/app.js
+++ js/app.js
@@ -6,18 +6,20 @@
     }));
 })(["jquery", "mu-jquery-capture/add", "mu-jquery-loom/jquery.loom", "mu-jquery-hub/hub"], this, function (jQuery, add, loom, hub) {
   var root = this;
-  var $event = jQuery.event;
 
   function load(module) {
     return root[module];
   }
 
-  $event.add = add.call(jQuery, $event.add);
+  // Replace jQuery.event.add with a version that captures results
+  jQuery.event.add = add(jQuery);
 
+  // Extend jQuery.fn with .crank/.twist/.weave
   loom.call(jQuery.fn, "[mu-widget]", "mu-widget", load, {
     "hub": hub.call(jQuery, "memory", "stopOnFalse")
   });
 
+  // Wait for document.ready and weave the document
   jQuery(function ($) {
     $(document).weave().fail(console.error.bind(console));
   });
